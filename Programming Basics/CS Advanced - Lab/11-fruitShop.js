5882d140b74beb8d397f69f4cee7e4308bd9f096
MeMo <131800476+darkCravings@users.noreply.github.com>
2023-06-13 17:36:38 +0100

    Update

--- Programming Basics/CS Advanced - Lab/11-fruitShop.js
+++ Programming Basics/CS Advanced - Lab/11-fruitShop.js
@@ -8,27 +8,29 @@ function shop(input) {
     if (day === 'Monday' || day === 'Tuesday' || day === 'Wednesday' || day === 'Thursday' || day === 'Friday') {
 
         switch (fruit) {
-            case 'banana': sum = qty * 2.50; console.log(sum.toFixed(2)); break;
-            case 'apple': sum = qty * 1.20; console.log(sum.toFixed(2)); break;
-            case 'orange': sum = qty * 0.85; console.log(sum.toFixed(2)); break;
-            case 'grapefruit': sum = qty * 1.45; console.log(sum.toFixed(2)); break;
-            case 'kiwi': sum = qty * 2.70; console.log(sum.toFixed(2)); break;
-            case 'pineapple': sum = qty * 5.50; console.log(sum.toFixed(2)); break;
-            case 'grapes': sum = qty * 3.85; console.log(sum.toFixed(2)); break;
-            default: console.log(`error`);
+            case 'banana': sum = qty * 2.50; break;
+            case 'apple': sum = qty * 1.20; break;
+            case 'orange': sum = qty * 0.85; break;
+            case 'grapefruit': sum = qty * 1.45; break;
+            case 'kiwi': sum = qty * 2.70; break;
+            case 'pineapple': sum = qty * 5.50; break;
+            case 'grapes': sum = qty * 3.85; break;
+            default: console.log(`error`); return;
         }
+        console.log(sum.toFixed(2));
 
     } else if (day === 'Saturday' || day === 'Sunday') {
         switch (fruit) {
-            case 'banana': sum = qty * 2.70; console.log(sum.toFixed(2)); break;
-            case 'apple': sum = qty * 1.25; console.log(sum.toFixed(2)); break;
-            case 'orange': sum = qty * 0.90; console.log(sum.toFixed(2)); break;
-            case 'grapefruit': sum = qty * 1.60; console.log(sum.toFixed(2)); break;
-            case 'kiwi': sum = qty * 3.00; console.log(sum.toFixed(2)); break;
-            case 'pineapple': sum = qty * 5.60; console.log(sum.toFixed(2)); break;
-            case 'grapes': sum = qty * 4.20; console.log(sum.toFixed(2)); break;
-            default: console.log(`error`); break;
+            case 'banana': sum = qty * 2.70; break;
+            case 'apple': sum = qty * 1.25; break;
+            case 'orange': sum = qty * 0.90; break;
+            case 'grapefruit': sum = qty * 1.60; break;
+            case 'kiwi': sum = qty * 3.00; break;
+            case 'pineapple': sum = qty * 5.60; break;
+            case 'grapes': sum = qty * 4.20; break;
+            default: console.log(`error`); return;
         }
+        console.log(sum.toFixed(2));
 
     } else {
         console.log(`error`);
